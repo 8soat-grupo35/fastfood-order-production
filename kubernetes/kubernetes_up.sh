c057193a355d33ef3ae65f7cc84109f714c78826
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2025-02-14 14:07:02 -0300

    feat: create CD process

--- kubernetes/kubernetes_up.sh
+++ kubernetes/kubernetes_up.sh
@@ -1,4 +1,4 @@
-docker build -t fastfood-app:latest .
-kubectl apply -f kubernetes/fastfood-deployment.yaml
-kubectl apply -f kubernetes/fastfood-service.yaml
-kubectl apply -f kubernetes/fastfood-hpa.yaml
+docker build -t fastfood-order-production-app:latest .
+kubectl apply -f kubernetes/fastfood-order-production-deployment.yaml
+kubectl apply -f kubernetes/fastfood-order-production-service.yaml
+kubectl apply -f kubernetes/fastfood-order-production-fastfood-hpa.yaml
